4a4810f31b0ea62dc0670855ccc4da3eddb37046
Paul Hammond <paul.hammond@bbc.co.uk>
2016-09-29 14:39:39 +0100

    return full scenario

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express = require('express')
 var app = express();
 var port = process.env.PORT || 3001;
+var glob = require('glob');
+var fs = require('fs');
+
+app.post('/scenario/:id', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+});
+
+app.get('/:scenario', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    glob('./mock-data/' + req.params.scenario + '/*.json', {}, function (er, files) {
+        res.json({
+            snapshots: files.map(function (file) {
+               return JSON.parse(fs.readFileSync(file, 'utf-8'));
+            })
+        });
+    })
+});
 
 app.get('/:scenario/:id', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
